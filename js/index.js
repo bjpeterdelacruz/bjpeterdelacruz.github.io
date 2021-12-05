2867f4efbd2a4b010433993276e8d10c40b2923d
BJ Peter Dela Cruz <bj.peter.delacruz@gmail.com>
2021-12-05 12:09:18 -0500

    Updated web page. Upgraded Font Awesome.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 function toggleDarkMode() {
     const bodyTag = document.getElementById("body");
+    const iTag = document.getElementById("lightbulb-icon");
+    const spanTag = document.getElementById("toggle-dark-mode-text");
     if (bodyTag.classList.contains("dark-mode")) {
         bodyTag.removeAttribute("class");
+        iTag.setAttribute("class", "fas fa-lightbulb");
+        spanTag.innerHTML = "Dark";
     }
     else {
         bodyTag.setAttribute("class", "dark-mode");
+        iTag.setAttribute("class", "fas fa-lightbulb-on");
+        spanTag.innerHTML = "Light";
     }
 }
