c02a549221622b0f37af45e63f6f0ea1770c0564
BJ Peter Dela Cruz <bj.peter.delacruz@gmail.com>
2021-12-10 20:08:37 -0500

    Updated styling of Codewars Katas page.

---
+++ js/common.js
@@ -0,0 +1,28 @@
+function toggleDarkMode() {
+    const body = document.getElementById("body");
+    const toggleDarkModeBtn = document.getElementById("toggle-dark-mode");
+    if (body.classList.contains("dark-mode")) {
+        body.removeAttribute("class");
+        toggleDarkModeBtn.innerHTML = `<i id="lightbulb-icon" class="fas fa-lightbulb"></i> Dark Mode`;
+        const headerRow = document.getElementById('katas-table-header-row');
+        if (headerRow) {
+            headerRow.setAttribute("class", "katas-row");
+            const list =  document.getElementById('katas-table-body').getElementsByTagName('tr');
+            for (let index = 0; index < list.length; index++) {
+                list[index].setAttribute("class", "katas-row");
+            }
+        }
+    }
+    else {
+        body.setAttribute("class", "dark-mode");
+        toggleDarkModeBtn.innerHTML = `<i id="lightbulb-icon" class="fas fa-lightbulb-on"></i> Light Mode`;
+        const headerRow = document.getElementById('katas-table-header-row');
+        if (headerRow) {
+            headerRow.setAttribute("class", "katas-row-dark-mode");
+            const list =  document.getElementById('katas-table-body').getElementsByTagName('tr');
+            for (let index = 0; index < list.length; index++) {
+                list[index].setAttribute("class", "katas-row-dark-mode");
+            }
+        }
+    }
+}
